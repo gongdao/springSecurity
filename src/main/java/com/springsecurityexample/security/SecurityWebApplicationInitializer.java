616252a73a7fa09b84858b17d40e87a40a233b78
buzhaobin <buzhaobin@gmail.com>
2016-04-13 20:10:10 -0400

    login showed.

---
+++ src/main/java/com/springsecurityexample/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.springsecurityexample.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+/*	public SecurityWebApplicationInitializer()
+	{
+		super(SecurityConfig.class);
+	}*/
+}
